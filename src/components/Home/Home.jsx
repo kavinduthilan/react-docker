b26cbab0ad22e692cc5911a3ca87b10f2751a69b
Kavindu Thilan <kavinduthilan2000@outlook.com>
2025-02-16 05:33:13 +0530

    added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Home() {
-  return <div>This is my Home</div>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>This is my Home</p>
+    </div>
+  );
 }
 
 export default Home;
